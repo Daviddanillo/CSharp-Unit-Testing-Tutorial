6d2a75cb6a05d0f59a6603f647a69e91c6929b04
Daviddanillo <daviddanillo07@gmail.com>
2026-09-14 19:50:36 -0300

    Introduz Regressao em Calculator.subtract

--- src/Library/Calculator.cs
+++ src/Library/Calculator.cs
@@ -7,7 +7,7 @@ namespace Library;
 public class Calculator
 {
     public int Add(int a, int b) => a + b;
-    public int Subtract(int a, int b) => a - b;
+    public int Subtract(int a, int b) => a + b;
     public int Multiply(int a, int b) => a * b;
 
     public double Divide(double a, double b)
